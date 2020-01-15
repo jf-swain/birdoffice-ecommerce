adb4a3e4c4ef646c9dc4b8e8ea2ba4931f789965
Jean-François Swain <swain.jean@gmail.com>
2020-01-15 19:12:08 +0100

    [MOD] (product) Create view

---
+++ src/components/AddProduct.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="action">
+    <div class="number">
+      <button>-</button>
+      <span>1</span>
+      <button>+</button>
+    </div>
+
+    <button class="add">Add to Cart</button>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  .action {
+    align-items: center;
+    display: flex;
+  }
+
+  .number {
+    align-items: center;
+    background: #E5E5E580;
+    border-radius: 3px;
+    display: flex;
+    justify-content: space-between;
+    padding: 6px;
+
+    button {
+      border: 1px solid #e5e5e5;
+      height: 3.6rem;
+      width: 3.6rem;
+    }
+
+    span {
+      color: color('secondary');
+      font-family: font-name('default-bold');
+      font-size: rem(16);
+      margin-left: 1.6rem;
+      margin-right: 1.6rem;
+    }
+  }
+
+  .add {
+    background-color: color('highlight');
+    border-radius: 3px;
+    border: 0;
+    color: color('basic-white');
+    font-family: font-name('default');
+    font-size: rem(16);
+    height: 51px;
+    margin-left: 2rem;
+    padding-left: 2.2rem;
+    padding-right: 2.2rem;
+  }
+</style>
---
+++ src/views/Product.vue
@@ -0,0 +1,140 @@
+<template>
+  <section>
+    <article>
+      <header>
+        <h2>Step behind the scenes of Havana’s theater</h2>
+        <span class="amount">eur 1,050.00</span>
+      </header>
+      <p>
+        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam
+      </p>
+
+      <p>
+        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+      </p>
+
+      <p class="annotate">
+        Prices valid till 31.10.2019, yearly adjustment for conversion rate to EURO
+      </p>
+      <add-to-cart />
+    </article>
+    <aside>
+      <img src="@/assets/images/product/product_1.jpg" alt="" />
+    </aside>
+  </section>
+</template>
+
+<script>
+  import AddProduct from '@/components/AddProduct';
+
+  export default {
+    components: {
+      AddToCart: AddProduct
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  section {
+    display: flex;
+    flex-direction: column;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 4rem;
+    max-width: $project-inner;
+    padding-left: $project-mobile-padding;
+    padding-right: $project-mobile-padding;
+
+    @include breakpoint('beyond-medium') {
+      flex-direction: row;
+      margin-top: 8.8rem;
+      padding-left: 0;
+      padding-right: 0;
+    }
+
+    article {
+      box-sizing: border-box;
+      order: 2;
+      padding-right: 4rem;
+      width: 100%;
+
+      @include breakpoint('beyond-medium') {
+        order: 1;
+        width: 62%;
+      }
+
+      header {
+        border-bottom: 13px solid rgba(color('highlight'), .10);
+        padding-bottom: 1.6rem;
+      }
+    }
+  }
+
+  h2 {
+    color: color('default');
+    font-family: font-name('default-black');
+    font-size: rem(24);
+    margin: 0;
+
+    @include breakpoint('beyond-project') {
+      font-size: rem(52);
+    }
+  }
+
+  .amount {
+    color: color('highlight');
+    display: block;
+    font-family: font-name('default');
+    font-size: rem(18);
+    margin-top: rem(16);
+    text-transform: uppercase;
+
+    @include breakpoint('beyond-project') {
+      font-size: rem(24);
+    }
+  }
+
+  p {
+    color: color('primary');
+    font-family: font-name('default-book');
+    font-size: rem(16);
+    margin-top: 3rem;
+
+    @include breakpoint('beyond-project') {
+      font-size: rem(18);
+    }
+  }
+
+  .annotate {
+    color: color('secondary');
+  }
+
+  aside {
+    backgound-repeat: no-repeat;
+    background-image: url('../assets/images/product/product_1.jpg');
+    background-position: center;
+    background-size: cover;
+    height: 15rem;
+    order: 1;
+    overflow: hidden;
+    width: 100%;
+
+    img {
+      display: none;
+
+      @include breakpoint('beyond-medium') {
+        display: block;
+      }
+    }
+
+    @include breakpoint('beyond-medium') {
+      height: auto;
+      order: 2;
+      width: 38%;
+    }
+
+    img {
+      width: 100%;
+    }
+  }
+</style>
